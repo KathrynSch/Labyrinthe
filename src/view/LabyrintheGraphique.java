3a3dbbb460a9da33c23f61f553eb8a29daf3e603
KATHRYN-LENOVO\kathr <kathryn.schutte@gmail.com>
2017-05-08 21:23:50 +0200

    update

---
+++ src/view/LabyrintheGraphique.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package view;
+import javax.swing.*;
+import java.awt.event.*;
+import model.Labyrinthe;
+
+/**
+ * @author kathr
+ *
+ */
+abstract class LabyrintheGraphique extends JFrame implements ActionListener{
+	static Labyrinthe laby;
+	
+}
